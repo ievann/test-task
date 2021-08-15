3457c1352f760fca8eb2a75450528b881d458c60
Ivan Novak <ievann.novak@gmail.com>
2021-08-15 11:11:30 +0400

    Finally working

--- csv_app/views.py
+++ csv_app/views.py
@@ -12,26 +12,22 @@ class IndexView(generic.list.ListView):
     template_name = 'index.html'
 
 def csv_upload(request):
+    data_entries = Data.objects.all()
+    template = "index.html"
+    prompt = {'order': 'CSV must have 14 columns', \
+            'data_entries': data_entries}
+    if request.method == "GET":
+        return render(request, template, prompt)
     if request.method == "POST":
         csv_file=request.FILES.get('csv', None).read().decode('UTF-8')
         io_string = io.StringIO(csv_file)
         next(io_string)
         for row in csv.reader(io_string, delimiter=';', quotechar='"'):
-            #print('ROW: ', row)
-            code = row[0]
-            name = row[1]
-            level1 = row[2]
-            level2 = row[3]
-            level3 = row[4]
-            price = row[5]
-            priceSP = row[6]
-            quantity = row[7]
-            value_fields = row[8]
-            joint_purchases = row[9]
-            measurment_unit = row[10]
-            picture = row[11]
-            show_on_mainpage = row[12]
-            description = row[13]
+            # Bulk variable declaration from row (14 pieces)
+            code, name, level1, level2, level3, price, priceSP, quantity, \
+                    value_fields, joint_purchases, measurment_unit, picture, \
+                    show_on_mainpage, description = row[:]
+            # Creating object with variables as Data model fields
             Data.objects.create(code=code, name=name,
                     #level1=level1, level2=level2, level3=level3,
                     price=price, priceSP=priceSP, quantity=quantity,
@@ -39,9 +35,10 @@ def csv_upload(request):
                     measurment_unit=measurment_unit, picture=picture,
                     show_on_mainpage=show_on_mainpage, description=description)
 
+        context = {'data_entries': data_entries}
         #response = render(request, "csv_upload.html")
-        #response = render(request, "index.html")
+        response = render(request, template, context)
         #response = redirect(IndexView.as_view())
-        response = HttpResponseRedirect('')
+        #response = HttpResponseRedirect('')
         #response = HttpResponse('')
         return response
